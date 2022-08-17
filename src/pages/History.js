669c3e1da00de20c61c82d95e0839eca98179f35
Jakson Moura <jrochelly@gmail.com>
2022-08-17 13:28:22 -0300

    Fix: remove unsed props

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const HistoryWrapper = styled.section`
