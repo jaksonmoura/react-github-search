865294622fc554c72b81723230c7285165c11841
Jakson Moura <jrochelly@gmail.com>
2022-08-17 13:10:36 -0300

    Fix: adjustments for not found

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,17 +10,19 @@ const Home = () => {
 	const defaultUsername = "jaksonmoura";
 	const dispatch = useDispatch();
 	const user = useSelector((state) => state.user);
-	let username = new URLSearchParams(document.location.search).get("username");
+	let username = new URLSearchParams(document.location.search).get("username") || "";
 
 	useEffect(() => {
-		if (username === user.login || user.login === defaultUsername) return;
+		if (user.message === "Not Found") return;
+		if (username === user.login && user.login === defaultUsername) return;
 		const handlePageLoad = async () => {
 			// if so, fetch and set it to the store
 			const newUser = await Api.fetchUserWithRepos(username || defaultUsername);
+			console.log(newUser.login);
 			dispatch(setCurrentUser(newUser));
 		};
 		handlePageLoad();
-	}, [username, user, dispatch]);
+	}, [username, user.login, user.message, dispatch]);
 
 	return (
 		<>
