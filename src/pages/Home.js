4f2c088b0a265336527750f5b5d2a26cedfcb14f
Jakson Moura <jrochelly@gmail.com>
2022-08-17 12:40:31 -0300

    Fix: call Api only when user is not the inital

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,26 @@
 import React, { useEffect } from "react";
 import Form from "../components/Form";
 import UserProfile from "../components/UserProfile";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setCurrentUser } from "../components/userSlice";
 import Api from "../Api";
 import UserRepositories from "../components/UserRepositories";
 
 const Home = () => {
+	const defaultUsername = "jaksonmoura";
 	const dispatch = useDispatch();
-
-	const handlePageLoad = async () => {
-		let urlParams = new URLSearchParams(document.location.search).get("username");
-		// check if url has username
-		const user = await Api.fetchUserWithRepos(urlParams || "jaksonmoura");
-		// if so, fetch and set it to the store
-
-		dispatch(setCurrentUser(user));
-	};
+	const user = useSelector((state) => state.user);
+	let username = new URLSearchParams(document.location.search).get("username");
 
 	useEffect(() => {
+		if (username === user.login || user.login === defaultUsername) return;
+		const handlePageLoad = async () => {
+			// if so, fetch and set it to the store
+			const newUser = await Api.fetchUserWithRepos(username || defaultUsername);
+			dispatch(setCurrentUser(newUser));
+		};
 		handlePageLoad();
-	});
+	}, [username, user, dispatch]);
 
 	return (
 		<>
