3f138896c9e364db0308087890b4ad34399c2f42
Jakson Moura <jrochelly@gmail.com>
2022-08-22 17:08:16 -0300

    Fix: Remove redudancy

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
 import historySlice from "./components/historySlice";
 import userSlice from "./components/userSlice";
 
@@ -14,6 +13,5 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default store;
